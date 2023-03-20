e70aac5cf95498c07d4bdc57be83417693fefee4
robmcman <mcman247@d.umn.edu>
2023-03-19 23:56:39 -0500

    Now filters for 15 years and Temp and DO ranges between 0-40

--- Lake_Merge_Script.R
+++ Lake_Merge_Script.R
@@ -76,6 +76,56 @@ library(lubridate)        #Allows me to extract a month out of my POSIX value
 All_Lakes_Aug <- All_Lakes_NoNA %>%
   filter(month(Date) == 8)
 
-#
+#Now follow methods from Jane et al
+
+#Removing all temp and DO values outside of 0-40
+
+summary(All_Lakes_Aug$DO) #This column has data outside of both bounds
+
+summary(All_Lakes$Temperature) #This column has data below 0º C
+
+#Subset for temp and DO to be within 0 and 40
+
+All_Lakes_Aug <- subset(All_Lakes_Aug, Temperature >= 0 & Temperature <= 40)
+summary(All_Lakes_Aug$Temperature) #Confirming
+
+All_Lakes_Aug <- subset(All_Lakes_Aug, DO >= 0 & DO <= 40)
+summary(All_Lakes_Aug$DO) #Confirming
+
+#Filtering so that locations are only included if they have a unique value for a minimum of 15 years:
+#Code is written so the years do not have to be consecutive 
+
+All_Lakes_Aug_Counts <- All_Lakes_Aug %>%
+  group_by(MonitoringLocationIdentifier, Year) %>% 
+  summarize(n_temps = sum(Temperature > 0)) %>% #calculates the number of temperature measurements that are greater than 0 for each combination of location and year
+  ungroup()
+
+
+All_Lakes_Aug_Counts <- All_Lakes_Aug_Counts %>%
+  group_by(MonitoringLocationIdentifier) %>%
+  summarize(n_years = sum(n_temps > 0)) %>% #calculates the number of years with at least one temperature measurement for each location
+  filter(n_years >= 15) %>% #Only include locations with >= 15 years
+  select(MonitoringLocationIdentifier) # Resulting df only lists locations that have 15 years of data
+
+
+All_Lakes_AugY <- subset(All_Lakes_Aug,
+                         MonitoringLocationIdentifier %in% All_Lakes_Aug_Counts$MonitoringLocationIdentifier)
+
+#The above code subsets All_Lakes_Aug by only locations that are also in the All_Lakes_Aug_Counts list 
+
+table(All_Lakes_AugY$State)
+#Remaining Locations:
+#Mi    MN    WI 
+#2065 30051 10651 
+
+
+
+
+
+
+
+
+
+
 
 
